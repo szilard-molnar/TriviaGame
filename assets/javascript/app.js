a3be6b4f25370ce71110bc0e664d8ca9e3add997
szilard-molnar <mszilard86@gmail.com>
2019-07-10 16:06:15 -0400

    pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// trivia game
+
+// displaying a title page with a start button.
+
+// on click the game page displays
+
+// all the questions will show below each other as separate divs, the answers will be radio buttons
+
+// there will be a counter that will give a certain amount of time for the player to pick the answers. when the counter hits 0: --> it will check if all the answers were clicked. if yes, it will check if they are correct, if yes, winning page displays. if any of these conditions won't match. the losing page comes up.
+
+// after the last question there will be a "DONE" button. pressing the "DONE" button will use the same function that the the counter does when it hits 0.
+
+// restart button that starts over again.
+
+// Bonus for me: if there is time, create the game in Hungarian too. Have a radio button that gives tha player the chance to pick another language option besides english.
